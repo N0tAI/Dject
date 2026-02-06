ccd67cfa062fa164507606a40a0d4905f4c97a30
NotAI <notai@notai.xyz>
2026-02-06 16:03:05 -0500

    Style: Removed some empty usings

--- src/AInjection.Library/ComponentRegistration.cs
+++ src/AInjection.Library/ComponentRegistration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AInjection
+﻿namespace AInjection
 {
 	public class ComponentRegistration(Type instanceType, Type[] types)
 	{
